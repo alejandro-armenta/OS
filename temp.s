f3e8c3338dd9691e863ae089b7216da10af7c50b
Alejandro <armentaayalaalejandro@gmail.com>
2026-04-19 20:09:29 -0600

    continued os

---
+++ temp.s
@@ -0,0 +1,41 @@
+
+db 'a', 'b', 'c'
+
+our_variable db 'abc', 0
+
+;alocate 100 bytes of memory
+times 100 db 0
+
+
+print_character_S_with_BIOS:
+
+    mov ah, 0Eh
+
+    mov al, 'S'
+
+    int 10h
+
+    ret
+
+;aqui no pasa nada
+
+    mov bx, 5
+
+loop_start:
+
+    cmp bx, 0
+
+    je loop_end
+
+    times 5 call print_character_S_with_BIOS
+
+    ;call print_character_S_with_BIOS
+
+    dec bx
+
+    jmp loop_start
+
+loop_end:
+
+    
+;
